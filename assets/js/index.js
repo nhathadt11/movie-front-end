4a2f77153d47c943d7e98fd5f70e8b88d45e4a27
Nhat Ha <nhathadt11@gmail.com>
2018-07-03 16:13:32 +0700

    Add pagination

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,12 +2,16 @@
 (function() {
   document.addEventListener('DOMContentLoaded', function() {
     fetchMovieStylesheet();
+    renderPagination(7, 1);
   });
 
   var BASE_URL = 'http://localhost:4567';
   var client = new http.SimpleHTTPClient();
 
-  fetchMoviesByPage(1);
+  fetchMoviesByPage(1, function(xml) {
+    displayMovies(xml);
+    setPageActive(1);
+  });
 
   function fetchMovieStylesheet() {
     client
@@ -20,10 +24,10 @@
     localStorage.setItem('movie.xsl', stylesheet);
   }
 
-  function fetchMoviesByPage(pageNumber) {
+  function fetchMoviesByPage(pageNumber, success) {
     client
       .get(BASE_URL + '/movies/page/' + pageNumber)
-      .after(displayMovies)
+      .after(success)
       .send();
   }
 
@@ -34,7 +38,14 @@
       var htmlMoviesFragment = transformToHtmlDocument(xml, xsl);
       var movieList = document.querySelector('.twelve.column.movie-list');
 
+      removeAllChildrenFrom(movieList);
       movieList.appendChild(htmlMoviesFragment);
+    }
+  }
+
+  function removeAllChildrenFrom(doc) {
+    while (doc.firstChild) {
+      doc.removeChild(doc.firstChild);
     }
   }
 
@@ -52,5 +63,52 @@
 
       return xsltProcessor.transformToFragment(xmlDoc, document);
     }
+  }
+
+  /**
+   * Pagination
+   */
+  function renderPagination(totalPage, activePage) {
+    var pagination = document.createElement('ul');
+    pagination.classList.add('pagination');
+
+    for (var i = 1; i <= totalPage; i++) {
+      var li = document.createElement('li');
+
+      li.classList.add('float-card', 'pagination-item')
+      li.innerText = i;
+
+      if (i === activePage) {
+        li.classList.add('activeadd')
+      } else {
+        li.classList.remove('active');
+      }
+
+      li.addEventListener('click', (function(page) {
+        return function() {
+          fetchMoviesByPage(page, function(xml) {
+            displayMovies(xml);
+            setPageActive(page);
+          });
+        }
+      })(i));
+
+      pagination.appendChild(li);
+    }
+
+    var footer = document.querySelector('.row.footer');
+    footer.appendChild(pagination);
+  }
+
+  function setPageActive(pageActive) {
+    var pages = document.querySelectorAll('.float-card.pagination-item');
+
+    pages.forEach(function(page) {
+      if (Number(page.textContent) === pageActive) {
+        page.classList.add('active');
+      } else {
+        page.classList.remove('active');
+      }
+    })
   }
 })();
